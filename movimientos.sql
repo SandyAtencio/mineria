bdb66a3f5c9e0074fe241fe3fe2c870360bd3533
Essa <sandres9017@gmail.com>
2018-09-29 14:32:46 -0500

    Se Adiciona la creación del usuario con sus privilegios ademas  la creación de las secuencias y tablas de hecho

--- movimientos.sql
+++ movimientos.sql
@@ -1,12 +1,17 @@
-/*SQL Dimensiones*/
+/* Creación del usiario / Esquema*/
+CREATE USER caracteristicasClienteFrecuente IDENTIFIED BY caracteristicasClienteFrecuente;
 
+/*Se asignan los privilegios de administrador*/
+GRANT connect, dba TO caracteristicasClienteFrecuente;
+
+/*Dimensiones*/
 CREATE TABLE d_producto(
     llave_producto  VARCHAR(10) CONSTRAINT d_pro_llav_pk    PRIMARY KEY,
     numero_producto VARCHAR(10) CONSTRAINT d_pro_num_nn     NOT NULL,
     presentacion    VARCHAR(20) CONSTRAINT d_pro_pre_nn     NOT NULL,
     valorVenta      VARCHAR(20) CONSTRAINT d_pro_val_nn     NOT NULL,
     tipoProduto     VARCHAR(10) CONSTRAINT d_pro_tip_nn     NOT NULL,
-)
+);
 
 CREATE TABLE d_sucursal(
     llave_sucursal  VARCHAR(10) CONSTRAINT d_sucursal_llav_pk   PRIMARY KEY,
@@ -16,7 +21,7 @@ CREATE TABLE d_sucursal(
     barrio          VARCHAR(50) CONSTRAINT d_suc_bar_nn         NOT NULL,
     ciudad          VARCHAR(50) CONSTRAINT d_suc_ciu_nn         NOT NULL,
     departamento    VARCHAR(50) CONSTRAINT d_suc_dep_nn         NOT NULL,
-)
+);
 
 CREATE TABLE d_cliente(
     llave_cliente   VARCHAR(10) CONSTRAINT d_cli_llav_pk    PRIMARY KEY,
@@ -33,7 +38,7 @@ CREATE TABLE d_cliente(
     tipo_empresa    VARCHAR(50) CONSTRAINT d_cli_temp_nn    NOT NULL,
     profesion       VARCHAR(50) CONSTRAINT d_cli_pro_nn     NOT NULL,
     barrio_empresa  VARCHAR(50) CONSTRAINT d_cli_baremp_nn  NOT NULL,
-)
+);
 
 CREATE TABLE d_dia(
     llave_dia       VARCHAR(10) CONSTRAINT d_dia_llav_pk    PRIMARY KEY,
@@ -43,4 +48,23 @@ CREATE TABLE d_dia(
     nombre_dia      VARCHAR(30) CONSTRAINT d_dia_nom_nn     NOT NULL,
     semestre        NUMBER(1)   CONSTRAINT d_dia_sem_nn     NOT NULL,
     festivo         NUMBER(1)   CONSTRAINT d_dia_fes_nn     NOT NULL,
-)
+);
+
+/* Sequencias */
+CREATE SEQUENCE seq_producto;
+
+CREATE SEQUENCE seq_sucursal;
+
+CREATE SEQUENCE seq_cliente;
+
+CREATE SEQUENCE seq_dia;
+
+/* Tabla de hecho */
+CREATE TABLE h_caracteristicas_cliente_frecuente(
+    llave_producto          VARCHAR(10) CONSTRAINT h_pro_llav_nn        NOT NULL CONSTRAINT h_pro_llav_fk       REFERENCES d_producto(llave_producto),
+    llave_sucursal          VARCHAR(10) CONSTRAINT h_sucursal_llav_nn   NOT NULL CONSTRAINT h_sucursal_llav_fk  REFERENCES d_sucursal(llave_sucursal),
+    llave_cliente           VARCHAR(10) CONSTRAINT h_cli_llav_nn        NOT NULL CONSTRAINT h_cli_llav_fk       REFERENCES d_cliente(llave_cliente),
+    llave_dia               VARCHAR(10) CONSTRAINT h_dia_llav_nn        NOT NULL CONSTRAINT h_dia_llav_fk       REFERENCES d_dia(llave_dia),
+    cantidadTotalProductos  NUMBER(5)   CONSTRAINT h_can_tot_pro_nn     NOT NULL,
+    valorTotalProductos     NUMBER(30)  CONSTRAINT h_val_tot_pro_nn     NOT NULL     
+);
